36babf322cb481e75151620d208c57ade663895c
Giska Saputra <giskasaputra2005@gmail.com>
2026-06-29 01:21:18 +0700

    icon

--- views/layouts/auth.php
+++ views/layouts/auth.php
@@ -12,6 +12,7 @@ $this->beginPage();
     <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?> | Aplikasi Kepegawaian</title>
+    <link rel="icon" type="image/png" href="<?= \yii\helpers\Url::to('@web/ui-assets/images/favicon.png') ?>">
     <?php $this->head() ?>
 </head>
 <body class="flex-md-row page-login">
